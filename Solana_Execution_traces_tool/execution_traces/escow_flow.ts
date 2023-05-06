e2a02f6380da83ea0441642fc8e46e3c7817d940
tvs111 <73558991+tkachenko0@users.noreply.github.com>
2023-05-07 00:47:31 +0200

    Solana Escow

---
+++ Solana_Execution_traces_tool/execution_traces/escow_flow.ts
@@ -0,0 +1,294 @@
+import {
+    Connection,
+    Keypair,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    SystemProgram,
+    Transaction,
+    TransactionInstruction,
+    clusterApiUrl,
+    sendAndConfirmTransaction,
+} from '@solana/web3.js';
+
+import {
+    buildBufferFromActionAndNumber,
+    generateKeyPair,
+    getPublicKeyFromFile,
+    getTransactionFees,
+} from './utils';
+
+import * as borsh from 'borsh';
+import path from 'path';
+import { Buffer } from 'buffer';
+
+const PROGRAM_KEYPAIR_PATH = path.resolve(__dirname, '../solana/dist/escow/escow-keypair.json');
+
+enum Action {
+    Initialize = 0,
+    Deposit = 1,
+    Pay = 2,
+    Refund = 3,
+}
+
+enum State {
+    WaitDeposit = 0,
+    WaitRecipient = 1,
+    Closed = 2,
+};
+
+class EscowInfo {
+    seller: Buffer = Buffer.alloc(32);
+    buyer: Buffer = Buffer.alloc(32);
+    amount: number = 0;
+    state: State = State.WaitDeposit;
+
+    constructor(fields: {
+        seller: Buffer,
+        buyer: Buffer,
+        amount: number,
+        state: State,
+    } | undefined = undefined) {
+        if (fields) {
+            this.seller = fields.seller;
+            this.buyer = fields.buyer;
+            this.amount = fields.amount;
+            this.state = fields.state;
+        }
+    }
+
+    static schema = new Map([
+        [EscowInfo, {
+            kind: 'struct', fields: [
+                ['seller', [32]],
+                ['buyer', [32]],
+                ['amount', 'u64'],
+                ['state', 'u8'],
+            ]
+        }],
+    ]);
+
+    static size = borsh.serialize(
+        EscowInfo.schema,
+        new EscowInfo(),
+    ).length
+}
+
+let feesForSeller = 0;
+let feesForBuyer = 0;
+
+async function main() {
+    const connection = new Connection(clusterApiUrl("testnet"), "confirmed");
+
+    const programId = await getPublicKeyFromFile(PROGRAM_KEYPAIR_PATH);
+    const kpSeller = await generateKeyPair(connection, 1);
+    const kpBuyer = await generateKeyPair(connection, 1);
+
+    console.log("programId: ", programId.toBase58());
+    console.log("seller:    ", kpSeller.publicKey.toBase58());
+    console.log("buyer:     ", kpBuyer.publicKey.toBase58());
+
+    // 0. Initialize
+    console.log("\n--- Initialize. Actor: the seller ---");
+    const amount = 0.1 * LAMPORTS_PER_SOL;
+    let stateAccountPublicKey = await initialize(
+        connection,
+        programId,
+        kpSeller,
+        kpBuyer.publicKey,
+        amount
+    );
+
+    // 1. Deposit money (the buyer deposits the amout equal to price)
+    console.log("\n--- Deposit. Actor: the buyer ---");
+    await deposit(
+        connection,
+        programId,
+        kpBuyer,
+        stateAccountPublicKey,
+        amount
+    );
+
+    // Chose if to pay or to refund
+    const choice: Action = Action.Pay;
+    switch (choice.valueOf()) {
+        case Action.Pay:     // 2. Payment
+            console.log("\n--- Pay. Actor: the buyer ---");
+            await pay(
+                connection,
+                programId,
+                kpBuyer,
+                stateAccountPublicKey,
+            );
+            break;
+
+        case Action.Refund:  // 2. Refund
+            console.log("\n--- Refund. Actor: the seller ---");
+            await refund(
+                connection,
+                programId,
+                kpSeller,
+                stateAccountPublicKey,
+            );
+            break;
+    }
+
+    // Costs
+    console.log("\n........");
+    console.log("Fees for seller: ", feesForSeller / LAMPORTS_PER_SOL, " SOL");
+    console.log("Fees for buyer:  ", feesForBuyer / LAMPORTS_PER_SOL, " SOL");
+    console.log("Total fees:      ", (feesForSeller + feesForBuyer) / LAMPORTS_PER_SOL, " SOL");
+}
+
+main().then(
+    () => process.exit(),
+    err => {
+        console.error(err);
+        process.exit(-1);
+    }
+);
+
+async function initialize(
+    connection: Connection,
+    programId: PublicKey,
+    kpSeller: Keypair,
+    buyerPubKey: PublicKey,
+    amount: number,
+): Promise<PublicKey> {
+
+    const SEED = "abcdef" + Math.random().toString();
+    const stateAccountPublicKey = await PublicKey.createWithSeed(kpSeller.publicKey, SEED, programId);
+
+    // Instruction to create the State Account account
+    const createStateAccountInstruction = SystemProgram.createAccountWithSeed({
+        fromPubkey: kpSeller.publicKey,
+        basePubkey: kpSeller.publicKey,
+        seed: SEED,
+        newAccountPubkey: stateAccountPublicKey,
+        lamports: await connection.getMinimumBalanceForRentExemption(EscowInfo.size),
+        space: EscowInfo.size,
+        programId: programId,
+    });
+
+    const initInstruction = new TransactionInstruction({
+        keys: [
+            { pubkey: kpSeller.publicKey, isSigner: true, isWritable: false },
+            { pubkey: buyerPubKey, isSigner: false, isWritable: false },
+            { pubkey: stateAccountPublicKey, isSigner: false, isWritable: true },
+        ],
+        programId,
+        data: buildBufferFromActionAndNumber(Action.Initialize, amount)
+    });
+
+    // Instruction to the program
+    const transaction = new Transaction().add(
+        createStateAccountInstruction,
+        initInstruction
+    );
+    await sendAndConfirmTransaction(connection, transaction, [kpSeller]);
+
+    let tFees = await getTransactionFees(transaction, connection);
+    feesForSeller += tFees;
+    console.log('    Transaction fees: ', tFees / LAMPORTS_PER_SOL, ' SOL');
+
+    return stateAccountPublicKey;
+}
+
+async function deposit(
+    connection: Connection,
+    programId: PublicKey,
+    kpBuyer: Keypair,
+    stateAccountPublicKey: PublicKey,
+    amount: number
+): Promise<void> {
+
+    // Instruction to transfer lamports to the State Account account
+    const transferLamportsToStateAccount = SystemProgram.transfer({
+        fromPubkey: kpBuyer.publicKey,
+        toPubkey: stateAccountPublicKey,
+        lamports: amount
+    });
+
+    // Instruction to the program
+    const depositInstruction = new TransactionInstruction({
+        keys: [
+            { pubkey: kpBuyer.publicKey, isSigner: true, isWritable: false },
+            { pubkey: stateAccountPublicKey, isSigner: false, isWritable: true },
+        ],
+        programId,
+        data: Buffer.from(new Uint8Array([Action.Deposit])),
+    });
+
+    const transaction = new Transaction().add(
+        transferLamportsToStateAccount,
+        depositInstruction
+    );
+    await sendAndConfirmTransaction(connection, transaction, [kpBuyer]);
+
+    let tFees = await getTransactionFees(transaction, connection);
+    feesForBuyer += tFees;
+    console.log('    Transaction fees: ', tFees / LAMPORTS_PER_SOL, ' SOL');
+}
+
+async function pay(
+    connection: Connection,
+    programId: PublicKey,
+    kpBuyer: Keypair,
+    stateAccountPublicKey: PublicKey,
+): Promise<void> {
+
+    const stateAccountInfo = await connection.getAccountInfo(stateAccountPublicKey);
+    if (stateAccountInfo === null) {
+        throw new Error('Error: cannot find the state account');
+    }
+    const stateInfo = borsh.deserialize(EscowInfo.schema, EscowInfo, stateAccountInfo.data,);
+
+    // Instruction to the program
+    const payInstruction = new TransactionInstruction({
+        keys: [
+            { pubkey: kpBuyer.publicKey, isSigner: true, isWritable: false },
+            { pubkey: new PublicKey(stateInfo.seller), isSigner: false, isWritable: true },
+            { pubkey: stateAccountPublicKey, isSigner: false, isWritable: true },
+        ],
+        programId,
+        data: Buffer.from(new Uint8Array([Action.Pay])),
+    });
+
+    const transaction = new Transaction().add(payInstruction);
+    await sendAndConfirmTransaction(connection, transaction, [kpBuyer]);
+
+    let tFees = await getTransactionFees(transaction, connection);
+    feesForBuyer += tFees;
+    console.log('    Transaction fees: ', tFees / LAMPORTS_PER_SOL, ' SOL');
+}
+
+async function refund(
+    connection: Connection,
+    programId: PublicKey,
+    kpSeller: Keypair,
+    stateAccountPublicKey: PublicKey,
+): Promise<void> {
+
+    const stateAccountInfo = await connection.getAccountInfo(stateAccountPublicKey);
+    if (stateAccountInfo === null) {
+        throw new Error('Error: cannot find the state account');
+    }
+    const stateInfo = borsh.deserialize(EscowInfo.schema, EscowInfo, stateAccountInfo.data,);
+
+    // Instruction to the program
+    const refundInstruction = new TransactionInstruction({
+        keys: [
+            { pubkey: kpSeller.publicKey, isSigner: true, isWritable: true },
+            { pubkey: new PublicKey(stateInfo.buyer), isSigner: false, isWritable: true },
+            { pubkey: stateAccountPublicKey, isSigner: false, isWritable: true },
+        ],
+        programId,
+        data: Buffer.from(new Uint8Array([Action.Refund])),
+    });
+
+    const transaction = new Transaction().add(refundInstruction);
+    await sendAndConfirmTransaction(connection, transaction, [kpSeller]);
+
+    let tFees = await getTransactionFees(transaction, connection);
+    feesForSeller += tFees;
+    console.log('    Transaction fees: ', tFees / LAMPORTS_PER_SOL, ' SOL');
+}
---
+++ Solana_Execution_traces_tool/solana/escow/src/lib.rs
@@ -0,0 +1,219 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+    match instruction_data[0] {
+        0 => initialize(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        ),
+        1 => deposit(program_id, accounts),
+        2 => pay(program_id, accounts),
+        3 => refund(program_id, accounts),
+        _ => {
+            msg!("Didn't found the entrypoint required");
+            Err(ProgramError::InvalidInstructionData)
+        }
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq)]
+enum State {
+    WaitDeposit = 0,
+    WaitRecipient = 1,
+    Closed = 2,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct EscowInfo {
+    pub seller: Pubkey,
+    pub buyer: Pubkey,
+    pub amount: u64,
+    pub state: State,
+}
+
+fn initialize(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter: &mut std::slice::Iter<AccountInfo> = &mut accounts.iter();
+    let seller: &AccountInfo = next_account_info(accounts_iter)?;
+    let buyer: &AccountInfo = next_account_info(accounts_iter)?;
+    let state_account: &AccountInfo = next_account_info(accounts_iter)?;
+
+    if !seller.is_signer {
+        msg!("The seller account should be the signer");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if state_account.owner != program_id {
+        msg!("The state account isn't owned by program");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let rent_exemption: u64 = Rent::get()?.minimum_balance(state_account.data_len());
+    if **state_account.try_borrow_lamports()? < rent_exemption {
+        msg!("The state account should be rent exempted");
+        return Err(ProgramError::InsufficientFunds);
+    }
+
+    let amount: u64 = instruction_data
+        .iter()
+        .rev()
+        .fold(0, |acc, &x| (acc << 8) + x as u64);
+
+    if amount <= 0 {
+        msg!("The amount should be positive");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let escow_info = EscowInfo {
+        seller: *seller.key,
+        buyer: *buyer.key,
+        amount,
+        state: State::WaitDeposit,
+    };
+
+    escow_info.serialize(&mut &mut state_account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
+}
+
+fn deposit(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let accounts_iter: &mut std::slice::Iter<AccountInfo> = &mut accounts.iter();
+    let buyer_account: &AccountInfo = next_account_info(accounts_iter)?;
+    let state_account: &AccountInfo = next_account_info(accounts_iter)?;
+
+    if !buyer_account.is_signer {
+        msg!("The buyer account should be the signer");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if state_account.owner != program_id {
+        msg!("The state account isn't owned by program");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let mut escow_info = EscowInfo::try_from_slice(*state_account.data.borrow())?;
+
+    if escow_info.buyer != *buyer_account.key {
+        msg!("Only the buyer can deposit");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let rent_exemption: u64 = Rent::get()?.minimum_balance(state_account.data_len());
+    if **state_account.try_borrow_lamports()? < escow_info.amount + rent_exemption {
+        msg!("Not enough lamports in the state account");
+        return Err(ProgramError::InsufficientFunds);
+    }
+
+    if escow_info.state != State::WaitDeposit {
+        msg!("The escow isn't in the state of waiting a deposit");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    escow_info.state = State::WaitRecipient;
+    escow_info.serialize(&mut &mut state_account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
+}
+
+fn pay(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let accounts_iter: &mut std::slice::Iter<AccountInfo> = &mut accounts.iter();
+    let buyer_account: &AccountInfo = next_account_info(accounts_iter)?;
+    let seller_account: &AccountInfo = next_account_info(accounts_iter)?;
+    let state_account: &AccountInfo = next_account_info(accounts_iter)?;
+
+    if !buyer_account.is_signer {
+        msg!("The buyer account should be the signer");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if state_account.owner != program_id {
+        msg!("The state account isn't owned by program");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let mut escow_info = EscowInfo::try_from_slice(*state_account.data.borrow())?;
+
+    if escow_info.buyer != *buyer_account.key {
+        msg!("Only the buyer can pay");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    if escow_info.state != State::WaitRecipient {
+        msg!("The escow isn't in the state of waiting the recipient");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    **seller_account.try_borrow_mut_lamports()? += **state_account.try_borrow_lamports()?;
+    **state_account.try_borrow_mut_lamports()? = 0;
+
+    escow_info.state = State::Closed;
+    escow_info.serialize(&mut &mut state_account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
+}
+
+fn refund(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let accounts_iter: &mut std::slice::Iter<AccountInfo> = &mut accounts.iter();
+    let seller_account: &AccountInfo = next_account_info(accounts_iter)?;
+    let buyer_account: &AccountInfo = next_account_info(accounts_iter)?;
+    let state_account: &AccountInfo = next_account_info(accounts_iter)?;
+
+    if !seller_account.is_signer {
+        msg!("The seller account should be the signer");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if state_account.owner != program_id {
+        msg!("The state account isn't owned by program");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    let mut escow_info = EscowInfo::try_from_slice(*state_account.data.borrow())?;
+
+    if escow_info.seller != *seller_account.key {
+        msg!("Only the seller can refund");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    if escow_info.state != State::WaitRecipient {
+        msg!("The escow isn't in the state of waiting the recipient");
+        return Err(ProgramError::InvalidAccountData);
+    }
+
+    msg!("state_account: {}", **state_account.try_borrow_lamports()?);
+
+    **buyer_account.try_borrow_mut_lamports()? += escow_info.amount;
+    **state_account.try_borrow_mut_lamports()? -= escow_info.amount;
+
+    // Return the rent founds to the seller
+    **seller_account.try_borrow_mut_lamports()? += **state_account.try_borrow_lamports()?;;
+    **state_account.try_borrow_mut_lamports()? = 0;
+
+    escow_info.state = State::Closed;
+    escow_info.serialize(&mut &mut state_account.try_borrow_mut_data()?[..])?;
+
+    Ok(())
+}
