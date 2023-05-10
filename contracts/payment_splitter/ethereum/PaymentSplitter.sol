0d5af6f09168f05e2a31cf02fff5b2e4ffc04369
Massimo Bartoletti <bart@unica.it>
2023-05-10 08:01:56 +0200

    added payment_splitter/ethereum

---
+++ contracts/payment_splitter/ethereum/PaymentSplitter.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (finance/PaymentSplitter.sol)
+
+pragma solidity ^0.8.0;
+
+contract PaymentSplitter {
+
+    uint256 private _totalShares;
+    uint256 private _totalReleased;
+
+    mapping(address => uint256) private _shares;
+    mapping(address => uint256) private _released;
+    address[] private _payees;
+
+    constructor(address[] memory payees, uint256[] memory shares_) payable {
+        require(payees.length == shares_.length, "PaymentSplitter: payees and shares length mismatch");
+        require(payees.length > 0, "PaymentSplitter: no payees");
+
+        for (uint256 i = 0; i < payees.length; i++) {
+            _addPayee(payees[i], shares_[i]);
+        }
+    }
+
+    receive() external payable virtual { }
+
+    function totalShares() public view returns (uint256) {
+        return _totalShares;
+    }
+
+    function totalReleased() public view returns (uint256) {
+        return _totalReleased;
+    }
+
+    function shares(address account) public view returns (uint256) {
+        return _shares[account];
+    }
+
+    function released(address account) public view returns (uint256) {
+        return _released[account];
+    }
+
+    function payee(uint256 index) public view returns (address) {
+        return _payees[index];
+    }
+
+    function releasable(address account) public view returns (uint256) {
+        uint256 totalReceived = address(this).balance + totalReleased();
+        return _pendingPayment(account, totalReceived, released(account));
+    }
+
+    function release(address payable account) public virtual {
+        require(_shares[account] > 0, "PaymentSplitter: account has no shares");
+
+        uint256 payment = releasable(account);
+
+        require(payment != 0, "PaymentSplitter: account is not due payment");
+
+        // _totalReleased is the sum of all values in _released.
+        // If "_totalReleased += payment" does not overflow, then "_released[account] += payment" cannot overflow.
+        _totalReleased += payment;
+        unchecked {
+            _released[account] += payment;
+        }
+
+        (bool success,) = account.call{value: payment}("");
+        require(success);
+    }
+
+    function _pendingPayment(
+			     address account,
+			     uint256 totalReceived,
+			     uint256 alreadyReleased
+    ) private view returns (uint256) {
+        return (totalReceived * _shares[account]) / _totalShares - alreadyReleased;
+    }
+
+    function _addPayee(address account, uint256 shares_) private {
+        require(account != address(0), "PaymentSplitter: account is the zero address");
+        require(shares_ > 0, "PaymentSplitter: shares are 0");
+        require(_shares[account] == 0, "PaymentSplitter: account already has shares");
+
+        _payees.push(account);
+        _shares[account] = shares_;
+        _totalShares = _totalShares + shares_;
+    }
+}
