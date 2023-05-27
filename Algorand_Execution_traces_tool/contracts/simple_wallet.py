dfac24bbe99f2e70f806b6e44eba5daf592db30f
petitnau <roberto.pettinau99@gmail.com>
2023-05-27 12:25:15 +0200

    Add comments to simple_wallet

--- Algorand_Execution_traces_tool/contracts/simple_wallet.py
+++ Algorand_Execution_traces_tool/contracts/simple_wallet.py
@@ -15,15 +15,23 @@ class TransactionRecord(pt.abi.NamedTuple):
     data: pt.abi.Field[pt.abi.DynamicBytes]
 
 class SimpleWalletState:
-    amount = GlobalStateValue(pt.TealType.uint64)
-    transactions = BoxMapping(pt.abi.Uint64, TransactionRecord, pt.Bytes("txn_"))
+    length = GlobalStateValue(
+        stack_type=pt.TealType.uint64,
+        descr="How many transactions are in state",
+    )
+    transactions = BoxMapping(
+        key_type=pt.abi.Uint64,
+        value_type=TransactionRecord,
+        prefix=pt.Bytes("txn_"),
+    )
 
 app = beaker.Application("Auction", state=SimpleWalletState())
 
 @app.create
 def create():
     return Seq(
-        app.state.amount.set(Int(0))
+        # Initialize length of the transactions "array"
+        app.state.length.set(Int(0))
     )
 
 @app.external
@@ -34,16 +42,20 @@ def create_transaction(
 ):
     return Seq(
         Assert(Txn.sender() == Global.creator_address(),
-               comment="Only the creator"),
+               comment="Only the creator can create transactions"),
         
+        # Create transaction object
         (executed := pt.abi.Bool()).set(FALSE),
         (to_ := pt.abi.Address()).set(to.get()),
         (value_ := pt.abi.Uint64()).set(value.get()),
         (data_ := pt.abi.DynamicBytes()).set(data.get()),
         (txn := TransactionRecord()).set(executed, to_, value_, data_),
-        app.state.transactions[pts.itoa(app.state.amount)].set(txn),
 
-        app.state.amount.set(app.state.amount + Int(1))
+        # Store it into the state
+        app.state.transactions[pts.itoa(app.state.length)].set(txn),
+
+        # Increase array length
+        app.state.length.set(app.state.length + Int(1))
     )
 
 @app.external
@@ -51,6 +63,7 @@ def execute_transaction(
     tx_id: pt.abi.Uint64,
 ): 
     return Seq(
+        # Obtain transaction object and its values
         (txn := TransactionRecord()).decode(
             app.state.transactions[pts.itoa(tx_id.get())].get()
         ),
@@ -58,12 +71,13 @@ def execute_transaction(
         (to := pt.abi.Address()).set(txn.to),
         (value := pt.abi.Uint64()).set(txn.value),
         (data := pt.abi.DynamicBytes()).set(txn.data),
-        
-        Assert(Txn.sender() == Global.creator_address(),                                 
-               comment="Only the creator"),
+
+        Assert(Txn.sender() == Global.creator_address(),
+               comment="Only the creator can execute transactions"),
         Assert(executed.get() == Int(0),
-               comment="Transaction already executed"),
+               comment="Transaction must not have been already executed"),
         
+        # Execute the transaction
         InnerTxnBuilder.Execute({
             TxnField.type_enum: TxnType.Payment,
             TxnField.receiver: to.get(),
@@ -72,6 +86,7 @@ def execute_transaction(
             TxnField.fee: Int(0),
         }),
         
+        # Remove it from the list
         pt.Pop(app.state.transactions[pts.itoa(tx_id.get())].delete()),
     )
 
@@ -79,8 +94,9 @@ def execute_transaction(
 def withdraw():
     return Seq(
         Assert(Txn.sender() == Global.creator_address(),                          
-               comment="Only the creator"),
+               comment="Only the creator can withdraw funds"),
         
+        # Execute the wthdrawal
         InnerTxnBuilder.Execute({
             TxnField.type_enum: TxnType.Payment,
             TxnField.close_remainder_to: Global.creator_address(),
@@ -88,5 +104,6 @@ def withdraw():
         })
     )
 
+
 if __name__ == "__main__":
-    app.build().export("auction_src")
+    app.build().export("artifacts")
