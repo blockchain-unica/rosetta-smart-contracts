360973aaef89b6a553bcbfe9a353d89d29f3b446
petitnau <roberto.pettinau99@gmail.com>
2023-05-27 12:24:13 +0200

    Add comments to vault

--- contracts/vault/algorand/vault.py
+++ contracts/vault/algorand/vault.py
@@ -10,25 +10,30 @@ class States:
 
 class VaultState:
     recovery = beaker.GlobalStateValue(
-        stack_type=pt.TealType.uint64,
+        stack_type=pt.TealType.bytes,
         static=True,
+        descr="Address of recovery account",
     )
     wait_time = beaker.GlobalStateValue(
         stack_type=pt.TealType.uint64,
         static=True,
-    )
-    
-    receiver = beaker.GlobalStateValue(
-        stack_type=pt.TealType.bytes,
+        descr="Time to be waited before withdraw finalization",
     )
     request_time = beaker.GlobalStateValue(
         stack_type=pt.TealType.uint64,
+        descr="Round in which the withdrawal was requested",
+    )
+    receiver = beaker.GlobalStateValue(
+        stack_type=pt.TealType.bytes,
+        descr="Receiving address of scheduled withdrawal",
     )
     amount = beaker.GlobalStateValue(
         stack_type=pt.TealType.uint64,
+        descr="Amount scheduled to be withdrawn",
     )
     state = beaker.GlobalStateValue(
         stack_type=pt.TealType.uint64,
+        descr="Contract logical state: IDLE/REQ",
     )
 
 app = beaker.Application("Vault", state=VaultState())
@@ -39,61 +44,86 @@ def create(
     wait_time_: pt.abi.Uint64,
 ):
     return pt.Seq(
-        app.state.recovery.set(recovery_),
-        app.state.wait_time.set(wait_time_),
+        # Initialize static parameters
+        app.state.recovery.set(recovery_.get()),
+        app.state.wait_time.set(wait_time_.get()),
+
+        # Transition to initial state
         app.state.state.set(States.IDLE),
     )
 
-@app.external(method_config=pt.MethodConfig(
-    opt_in=pt.CallConfig.ALL,
-    no_op=pt.CallConfig.ALL,
-))
-def donate(
-    donation: pt.abi.PaymentTransaction,
+@app.external
+def withdraw(
+    amount_: pt.abi.Uint64,
+    receiver_: pt.abi.Address,
 ):
-    """
-    anyone can transfer native cryptocurrency to the contract until the deadline;
-    """
     return Seq(
-        Assert(donation.get().receiver() == Global.current_application_address()),
-        Assert(Global.round() <= app.state.end_donate),
-        app.state.total_donated.set(app.state.total_donate + donation.get().amount()),
-        app.state.donated[Txn.sender()].set(app.state.donate[Txn.sender()] + donation.get().amount()),
+        Assert(app.state.state == States.IDLE,
+               comment="No withdrawal request in progress"),
+        Assert(app.state.amount <=  pt.Balance(Global.current_application_address()),
+               comment="Amount to be withdrawn is available"),
+        Assert(Txn.sender() == Global.creator_address(),
+               comment="Creator requested the withdraw"),
+        
+        # Set withdrawal information
+        app.state.request_time.set(Global.round()),
+        app.state.amount.set(amount_.get()),
+        app.state.receiver.set(receiver_.get()),
+
+        # Transition to REQ state (withdrawal in progress)
+        app.state.state.set(States.REQ),
     )
 
 @app.external
-def withdraw():
-    """
-    after the deadline, the recipient can withdraw the funds stored in the contract, provided that the goal has been reached;
-    """
+def finalize():
     return Seq(
-        Assert(Global.round() > app.state.end_donate),
-        Assert(app.state.total_donated >= app.state.goal),
-        InnerTxnBuilder.Execute({
-            TxnField.type_enum: TxnType.Payment,
-            TxnField.amount: app.state.total_donated,
-            TxnField.receiver: Txn.sender(),
-            TxnField.fee: Int(0),
-        }),
-        app.state.total_donated.set(Int(0)),
+        Assert(app.state.state == States.REQ,
+               comment="Withdrawal request in progress"),
+        Assert(Global.round() >= app.state.request_time + app.state.wait_time,
+               comment="Enough time has passed"),
+        Assert(Txn.sender() == Global.creator_address(),
+               comment="Only the creator can authorize the withdrawal finalization"),
+
+        # Finalize withdrawal
+        pay_or_close(app.state.receiver, app.state.amount),
+
+        # Transition to IDLE state (wait for withdrawal)
+        app.state.state.set(States.IDLE),
     )
 
-@app.close_out
-def reclaim():
-    """after the deadline, if the goal has not been reached donors can withdraw the amounts they have donated.
-    """
+@app.external
+def cancel():
     return Seq(
-        Assert(Global.round() > app.state.end_donate),
-        Assert(app.state.total_donated < app.state.goal),
-        Assert(app.state.donated[Txn.sender()] > 0),
-        InnerTxnBuilder.Execute({
-            TxnField.type_enum: TxnType.Payment,
-            TxnField.amount: app.state.donated[Txn.sender()],
-            TxnField.receiver: Txn.sender(),
-            TxnField.fee: Int(0),
-        }),
-        app.state.donated[Txn.sender()].set(Int(0)),
+        Assert(app.state.state == States.REQ,
+               comment="Withdrawal request in progress"),
+        Assert(Txn.sender() == app.state.recovery,
+               comment="Only the recovery account can cancel a withdrawal"),
+
+        # Transition to IDLE state (wait for withdrawal)
+        app.state.state.set(States.IDLE),
     )
+    
+@pt.Subroutine(pt.TealType.none)
+def pay_or_close(receiver, amt):
+    return (
+        # If full balance is being transferred, close
+        pt.If(pt.Balance(Global.current_application_address()) == amt).Then(
+            InnerTxnBuilder.Execute({
+                TxnField.type_enum: TxnType.Payment,
+                TxnField.close_remainder_to: receiver,
+                TxnField.fee: Int(0),
+            })
+        # Otherwise, just send the specified amount
+        ).Else(
+            InnerTxnBuilder.Execute({
+                TxnField.type_enum: TxnType.Payment,
+                TxnField.receiver: receiver,
+                TxnField.amount: amt,
+                TxnField.fee: Int(0),
+            })
+        )
+    )
+
 
 if __name__ == "__main__":
-    app.build().export("crowdfunding_src")
+    app.build().export("artifacts")
