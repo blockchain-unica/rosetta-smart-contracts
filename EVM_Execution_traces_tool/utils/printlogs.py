c3888ec2678ce92f3041cceddd3b262a517bc3cf
Massimo Bartoletti <bart@unica.it>
2024-03-30 11:07:56 +0100

    removes fees scripts

--- EVM_Execution_traces_tool/utils/printlogs.py
+++
@@ -1,9 +0,0 @@
-
-def print_receipt(tx_receipt):
-    print('contractAddress =', tx_receipt["contractAddress"])
-    print('cumulativeGasUsed =', tx_receipt['cumulativeGasUsed'])
-    #print('gasPrice Mainet =', gasprice)
-    #print('effectiveGasPrice =', tx_receipt['effectiveGasPrice'])
-    print('gasUsed =', tx_receipt['gasUsed'])
-    #print('COST(USD) =', gasprice*tx_receipt['gasUsed']*etherPrice.getEtherPrice()*(10**-9))
-
