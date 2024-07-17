962c8e83051c6172cb883aae5efb3bb9e56dd9cd
Massimo Bartoletti <bart@unica.it>
2024-07-17 18:28:36 +0200

    revised join0 in ethereum/lottery.sol

--- contracts/lottery/ethereum/lottery.sol
+++ contracts/lottery/ethereum/lottery.sol
@@ -14,29 +14,29 @@ contract Lottery {
     string secret0;
     string secret1;
 
+    uint256 bet_amount;
+
     enum Status {
         Join0,
         Join1,
         Commit0,
         Commit1,
-	Reveal0,
-	Reveal1,
-	Win,
+	    Reveal0,
+	    Reveal1,
+	    Win,
         End
     }
 
     uint end_join;
     uint end_reveal;
     
-    // Default value is the first element listed in
-    // definition of the type, in this case "Pending"
     Status public status;
     
     constructor() {
         owner = msg.sender;
-	status = Status.Join0;
-	end_join = block.number + 1000;
-	end_reveal = end_join + 1000;	
+	    status = Status.Join0;
+	    end_join = block.number + 1000;
+	    end_reveal = end_join + 1000;	
     }
 
     function join0(bytes32 h) payable public {
@@ -44,15 +44,16 @@ contract Lottery {
 
         player0 = payable(msg.sender);
         hash0 = h;
-	status = Status.Join1;
+	    status = Status.Join1;
+        bet_amount = msg.value;
     }
 
     function join1(bytes32 h) payable public {
-        require (status==Status.Join1 && h!=hash0 && msg.value > .01 ether);
+        require (status==Status.Join1 && h!=hash0 && msg.value == bet_amount);
 
         player1 = payable(msg.sender);
         hash1 = h;	
-	status = Status.Reveal0;
+    	status = Status.Reveal0;
     }
 
     function redeem0_nojoin1() public {
